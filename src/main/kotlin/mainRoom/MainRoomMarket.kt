5082c2173e084821dd7e628246f7c1024da6fe62
Solonichniy Volodimir <vsolo0@gmail.com>
2019-08-13 01:10:49 +0300

    0.83 bug market

--- src/main/kotlin/mainRoom/MainRoomMarket.kt
+++ src/main/kotlin/mainRoom/MainRoomMarket.kt
@@ -5,17 +5,28 @@ import screeps.api.Market
 import screeps.api.ORDER_BUY
 import screeps.api.RESOURCE_ENERGY
 
+fun MainRoom.marketClearNonActive() {
+    val orders: Array<Market.Order> = Game.market.getAllOrders {
+        (it.roomName == this.name
+                && it.resourceType == RESOURCE_ENERGY)
+                && !it.active
+    }
+    for (order in orders) Game.market.cancelOrder(order.id)
+
+}
+
 fun MainRoom.marketCreateBuyOrders() {
     if (!this.constant.marketBuyEnergy) return
-    val orders: Array<Market.Order> = Game.market.getAllOrders {it.roomName == this.name && it.resourceType == RESOURCE_ENERGY }
-    if (orders.isNotEmpty()) {
-        if (this.getResourceInTerminal() + this.getResourceInStorage() < 300000){
-            val order = orders.firstOrNull()
-            if (order != null) {
-                if (order.remainingAmount < 50000) Game.market.extendOrder(order.id,50000)
-            }
-        }
-        return
+    //this.marketClearNonActive()
+    if ((this.getResourceInTerminal() + this.getResourceInStorage()) < 300000) {
+        val orders: Array<Market.Order> = Game.market.getAllOrders { it.roomName == this.name && it.resourceType == RESOURCE_ENERGY }
+        if (orders.isNotEmpty()) {
+            val remainingAmount = orders.sumBy { it.remainingAmount }
+            if (remainingAmount < 50000) Game.market.extendOrder(orders[0].id, 50000)
+        } else
+            Game.market.createOrder(ORDER_BUY, RESOURCE_ENERGY, 0.012, 100000, this.name)
     }
-    Game.market.createOrder(ORDER_BUY, RESOURCE_ENERGY,0.012,200000,this.name)
+
+//    val orders: Array<Market.Order> = Game.market.getAllOrders { it.roomName == this.name && it.resourceType == RESOURCE_ENERGY }
+//    for (order in orders) console.log("Order id: ${order.id} remainingAmount: ${order.remainingAmount}")
 }
